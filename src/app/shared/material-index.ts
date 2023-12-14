97c3e8a4035554151ad0f199e4fa8170b521c229
Stavros Anastopoulos <astavros@vmware.com>
2023-12-14 17:23:56 +0200

    material 15

--- src/app/shared/material-index.ts
+++ src/app/shared/material-index.ts
@@ -1,8 +1,8 @@
-import { MatTableModule } from '@angular/material/table';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
 import { MatIconModule } from '@angular/material/icon';
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
-import { MatMenuModule } from '@angular/material/menu';
+import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
 
 export const Modules = [
     MatIconModule,
